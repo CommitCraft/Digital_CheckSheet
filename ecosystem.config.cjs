ba1d3a5518d2b9b812dfb902cdfa5ebc6ec3f236
aploslogix <techsaral84@gmail.com>
2026-02-23 11:36:38 +0530

    ip updated

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -14,12 +14,12 @@ module.exports = {
   // ⬇️ your env here
   env: {
     NODE_ENV: 'development',
-    VITE_HOST: '192.168.100.7',
+    VITE_HOST: '192.168.1.38',
     VITE_PORT: '8800'
   },
   env_production: {
     NODE_ENV: 'production',
-    VITE_HOST: '192.168.100.7',
+    VITE_HOST: '192.168.1.38',
     VITE_PORT: '8800'
   },
 
