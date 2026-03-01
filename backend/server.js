5a800f51b56cb93cb735d7265f69f4572247dae7
aploslogix <techsaral84@gmail.com>
2026-03-01 11:15:43 +0530

    update template CRUD checked

--- backend/server.js
+++ backend/server.js
@@ -55,7 +55,7 @@ app.use(cors({
     callback(new Error(`CORS blocked: ${origin}`));
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE',"PATCH", 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 }));
 
